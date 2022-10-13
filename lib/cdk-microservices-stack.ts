f71494b05e279f7a268279718d661695e8a97854
Kingsley Ijomah <git-example@codehance>
2022-10-12 20:34:40 -0400

    lambda function

--- lib/cdk-microservices-stack.ts
+++ lib/cdk-microservices-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class CdkMicroservicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -16,5 +18,22 @@ export class CdkMicroservicesStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
     });
+
+    // create product nodejsfunction
+    const productFunction = new NodejsFunction(this, 'productFunction', {
+      entry: 'src/product/product.ts',
+      handler: 'handler',
+      environment: {
+        TABLE_NAME: productTable.tableName,
+        PRIMARY_KEY: 'id',
+      },
+      bundling: {
+        externalModules: ['aws-sdk'],
+      },
+      runtime: Runtime.NODEJS_14_X,
+    });
+
+    // grant readwrite permission
+    productTable.grantReadWriteData(productFunction);
   }
 }
